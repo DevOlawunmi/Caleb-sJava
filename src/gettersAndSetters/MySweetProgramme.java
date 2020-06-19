8f848ae378acf2a6971a833478bce8c47974f95e
Ola <olawunmi263@yahoo.com>
2020-06-19 04:26:03 +0100

    Getters and Setters Lesson

---
+++ src/gettersAndSetters/MySweetProgramme.java
@@ -0,0 +1,11 @@
+package gettersAndSetters;
+
+public class MySweetProgramme {
+    public static void main(String[] args) {
+User user = new User();
+user.setFirstName("Abolade");
+user.setLastName("Oshodi");
+        System.out.println(user.getFirstName() + " " + user.getLastName());
+
+    }
+}
---
+++ src/gettersAndSetters/User.java
@@ -0,0 +1,20 @@
+package gettersAndSetters;
+
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName(){
+        return firstName.toUpperCase();
+    }
+    public void setFirstName(String fn){
+        firstName = fn;
+    }
+    public String getLastName (){
+        return lastName;
+    }
+    public void setLastName(String ln){
+        lastName = ln;
+    }
+
+}
