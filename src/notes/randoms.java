312819e73bb0ee445ed34b5190729af46bd77d18
Ola <olawunmi263@yahoo.com>
2020-06-26 06:47:44 +0100

    TAU For Loop added

--- src/notes/randoms.java
+++ src/notes/randoms.java
@@ -8,15 +8,12 @@ package notes;
 
 
 
-
-
-
-
-
-
-
-
  */
 
 public class randoms {
-}
+    public static void main(String[] args) {
+        int noOfApples = 5;
+        String name = "Olusola";
+        System.out.println(name.toUpperCase());
+        }
+    }
