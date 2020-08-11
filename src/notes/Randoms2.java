9ea61d71507e779b7aa8fbc2f9a3ae610b290700
Ola <olawunmi263@yahoo.com>
2020-08-11 04:21:42 +0100

    Revision

--- src/notes/Randoms2.java
+++ src/notes/Randoms2.java
@@ -6,7 +6,7 @@ public class Randoms2 {
     public static void main(String[] args) {
        // Randoms_WhileStatement t = new Randoms_WhileStatement();
       //  t.tacos();
-        
+
 
     }
 }
